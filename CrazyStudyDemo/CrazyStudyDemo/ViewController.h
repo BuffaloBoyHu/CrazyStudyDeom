5346448431ca2d6c6d423f901978524127c861ba
Buffaloboyhu <buffaloboyhu@163.com>
2016-04-29 09:08:59 +0800

    疯狂iOS自己的学习案例

---
+++ CrazyStudyDemo/CrazyStudyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrazyStudyDemo
+//
+//  Created by Buffalo on 16/4/29.
+//  Copyright © 2016年 Buffalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CrazyStudyDemo/CrazyStudyDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CrazyStudyDemo
+//
+//  Created by Buffalo on 16/4/29.
+//  Copyright © 2016年 Buffalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
